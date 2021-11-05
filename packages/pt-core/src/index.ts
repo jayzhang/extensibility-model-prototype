fa4fdd14d9d3a18e8540ad6002aad25350da74c7
Long Hao <haolong@microsoft.com>
2021-11-05 10:15:59 +0000

    chore: plugin debugging workflow

--- packages/pt-core/src/index.ts
+++ packages/pt-core/src/index.ts
@@ -1,18 +1,42 @@
 import "reflect-metadata";
 
-import { Container, Service, Inject } from "typedi";
+import { Container } from "typedi";
 import * as fs from "fs-extra";
+import * as path from "path";
 
-const pj = JSON.parse(fs.readFileSync("./package.json", { encoding: "utf8" }));
+const util = require("util");
+const exec = util.promisify(require("child_process").exec);
 
-const plugins = pj.plugins;
-
-async function operation() {
-  for (let p in plugins) {
-    await require(p);
-    console.log(`Plugin on board: ${Container.has(plugins[p].name)}`);
-  }
-  return;
+interface plugin {
+  foo: () => void;
 }
 
-operation().then(() => {});
+export async function operation(pluginPath?: string, pluginName?: string) {
+  const pj = JSON.parse(
+    fs.readFileSync(path.join(__dirname, "..", "package.json"), {
+      encoding: "utf8",
+    })
+  );
+
+  const plugins = pj.plugins;
+
+  for (let p in plugins) {
+    await require(p);
+    console.log(
+      `Buildin Plugin ${plugins[p].name}: ${Container.has(plugins[p].name)}`
+    );
+    const service = Container.get(plugins[p].name) as plugin;
+    service.foo();
+  }
+
+  if (pluginPath && pluginName) {
+    const { stdout, stderr } = await exec(`npm i ${pluginPath}`);
+    console.log(stdout);
+    console.log(stderr);
+
+    await require(pluginPath);
+    console.log(`Dynamic Plugin ${pluginName}: ${Container.has(pluginName)}`);
+    const service = Container.get(pluginName) as plugin;
+    service.foo();
+  }
+}
